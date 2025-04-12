07ef5c85f82e04102ef3d5e4dad31f952e4bc2a1
veyrr2 <gorokhov_d@iuca.kg>
2025-04-12 20:10:01 +0600

    modif: theme toggle ver 2

--- js/theme_toggle.js
+++ js/theme_toggle.js
@@ -1,14 +1,20 @@
-// Получаем ссылку на кнопку переключения темы по её ID
-const but_themeToggle = document.getElementById('but_night_theme_toggle');
-// Получаем ссылку на элемент <body>, чтобы менять его классы
+const but_themeToggle = document.getElementById('but_night_theme_toggle'); // кнопка смены темы
+const maing_logo = document.getElementById('main_logo'); // Ссылка на логотип
+const header_right_triangle = document.getElementById('header_right_triangle'); // правый треугольник в хидере
 const body = document.body;
-// Пытаемся получить сохраненную тему из localStorage
-const savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem('theme'); // Сохраненная тема из localStorage
 
 // Если тема была сохранена ранее (savedTheme не null и не undefined)
 if (savedTheme) {
     // Добавляем сохраненный класс темы к элементу <body>
     body.classList.add(savedTheme);
+    
+    // Если тема тёмная, обновляем изображения
+    if (savedTheme === 'dark_theme') {
+        but_themeToggle.src = "images/images for page style/but_theme_toggle_to_light.png"
+        maing_logo.src = "images/images for page style/cyanidewatch_logo_dark.png";
+        header_right_triangle.src = "images/images for page style/triangle-right-dark.PNG";
+    }
 }
 
 // Добавляем обработчик события 'click' с стрелочной функцией на кнопку переключения темы
@@ -16,13 +22,27 @@ but_themeToggle.addEventListener('click', () => {
     // Переключаем класс 'dark_theme' у элемента <body> (добавляем или удаляем его)
     body.classList.toggle('dark_theme');
 
-    // Если элемент <body> теперь содержит класс 'dark_theme' (темная тема активна)
+    // Если включена темная тема
     if (body.classList.contains('dark_theme')) {
+        // Для кнопки переключения темы сменить фото на темное
+        but_themeToggle.src = "images/images for page style/but_theme_toggle_to_light.png"
+        // для логотипа переключить на темную тему 
+        maing_logo.src = "images/images for page style/cyanidewatch_logo_dark.png";
+        // для правого треугольника менять тему
+        header_right_triangle.src = "images/images for page style/triangle-right-dark.PNG";
+
         // Сохраняем 'dark_theme' в localStorage, чтобы при следующей загрузке страницы тема сохранилась
         localStorage.setItem('theme', 'dark_theme');
-    } else {
-        // Если 'dark_theme' был удален (светлая тема активна), удаляем запись из localStorage
-        // Установка пустой строки, эквивалентна удалению элемента.
+    } 
+    else {
+        // Для кнопки переключения темы сменить фото на светлое
+        but_themeToggle.src = "images/images for page style/but_theme_toggle_to_dark.png"
+        // для логотипа переключить на светлую тему 
+        maing_logo.src = "images/images for page style/cyanidewatch_logo_light.png";
+        // для правого треугольника менять тему
+        header_right_triangle.src = "images/images for page style/triangle-right-light.PNG";
+
+        // Сменить на светлую тему путём удаления dark-theme заменой пустой строкой.
         localStorage.setItem('theme', '');
     }
 });

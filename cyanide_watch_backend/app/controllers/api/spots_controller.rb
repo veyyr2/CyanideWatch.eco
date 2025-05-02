ff10453de1cf8b9aa7d8a80af6e8202e2e4e5671
veyrr2 <gorokhov_d@iuca.kg>
2025-05-02 13:34:43 +0600

    fix: spots_controller (now it works)

--- cyanide_watch_backend/app/controllers/api/spots_controller.rb
+++ cyanide_watch_backend/app/controllers/api/spots_controller.rb
@@ -1,11 +1,8 @@
-class Api::SpotsController < ApplicationController
-    module Api
-        class SpotsController < ApplicationController
-          # Здесь вы добавляете методы контроллера
-          def index
-            spots = Spot.all
-            render json: spots
-          end
-        end
-      end
+module Api
+  class SpotsController < ApplicationController
+    def index
+      spots = Spot.all
+      render json: spots
+    end
+  end
 end
